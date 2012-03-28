e831521298a04b6b29aee53c09470e35ab4bd594
Andrew Hautau <arhautau@gmail.com>
2012-03-28 01:48:15 -0700

    Streamgraph of all datasets loads with page. Also figured out how to set the scale with each transition. Need to figure out scoping issues in the javscript in order to transition to a new graph with each query.

--- app/assets/javascripts/public/streamgraph_layers.js
+++ app/assets/javascripts/public/streamgraph_layers.js
@@ -1,3 +1,8 @@
+var area = d3.svg.area()
+    .x(function(d) { return d.x * w / mx; })
+    .y0(function(d) { return h - d.y0 * h / my; })
+    .y1(function(d) { return h - (d.y + d.y0) * h / my; });
+
 function getCountriesAndNids() {
     url = '/terms_by_parent/';
     result = {};
@@ -17,6 +22,59 @@ function getCountriesAndNids() {
 };
 
 
+
+function getStreamGraph(x) {
+var n = x.length, // number of layers
+    m = x[0].length, // number of samples per layer
+    data0 = d3.layout.stack().offset("wiggle")(x),
+    color = d3.interpolateRgb("#C8F099", "#31A635");
+
+var w = 1000,
+    h = 500,
+    mx = m - 1,
+    my = d3.max(data0, function(d) {
+      return d3.max(d, function(d) {
+        return d.y0 + d.y;
+      });
+    });
+
+var area = d3.svg.area()
+    .x(function(d) { return d.x * w / mx; })
+    .y0(function(d) { return h - d.y0 * h / my; })
+    .y1(function(d) { return h - (d.y + d.y0) * h / my; });
+
+var vis = d3.select("#chart")
+  .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+vis.selectAll("path")
+    .data(data0)
+  .enter().append("path")
+    .style("fill", function() { return color(Math.random()); })
+    .attr("d", area);
+
+showChart();
+
+}
+
+function transition(x) {
+  d3.selectAll("path")
+      .data(function() {
+       var d = x; 
+       return data0 = d;
+      })
+    .transition()
+      .duration(2500)
+      .attr("d", area);
+    my = d3.max(data0, function(d) {
+      return d3.max(d, function(d) {
+        return d.y0 + d.y;
+      });
+    });
+};
+
+
 function streamGraphTheData(data_array) {
 var x = new Array(data_array.length);
 for (var i = 0; i < data_array.length; i++) {
@@ -32,7 +90,10 @@ $.map(data_array, function(arr, index) {
 	});
 	return x;
 });
+return x;
 }
+
+
 
 
 
@@ -40,8 +101,12 @@ function showChart() {
 $('#chart').fadeIn("slow");
 };
 
-function hideChart() {
-$('#chart').fadeOut("slow");
+function hideChartTitle() {
+$('#chart #chart-title').fadeOut("slow");
+};
+
+function showChartTitle() {
+$('#chart #chart-title').fadeIn("slow");
 };
 
 function showLoadingGif() {
@@ -52,7 +117,12 @@ function hideLoadingGif() {
 $('.side-things img').fadeOut("slow");
 };
 
+
+
+
+
 jQuery(document).ready(function($) {
+
 
     alldatasetssurl = '/all_datasets_by_year/';
     all_datasets_array = {};
@@ -63,12 +133,17 @@ jQuery(document).ready(function($) {
         dataType: 'json',
         success: function(o){
             all_datasets_array = o;
+
+        x = streamGraphTheData(all_datasets_array);						
+        getStreamGraph(x);
+
          },
          error: function (xhr, ajaxOptions, thrownError){
              console.log(thrownError);
          }
-    });
-    
+    }); 
+
+
     alltypesurl = '/all_dataset_types/';
     all_types_array = {};
     $.ajax({
@@ -79,12 +154,11 @@ jQuery(document).ready(function($) {
         success: function(o){
             all_types_array = o;
             hideLoadingGif();
-            showChart();
          },
          error: function (xhr, ajaxOptions, thrownError){
              console.log(thrownError);
          }
-    });
+    });	
 
 
     countries = $.map(getCountriesAndNids(), function(val, index) {
@@ -96,9 +170,13 @@ jQuery(document).ready(function($) {
         items: 5,
     });
 
+
+
+
+
 $("#countryform").submit(function() {
   showLoadingGif();
-  hideChart();
+  hideChartTitle();
   $.map(getCountriesAndNids(), function(val, index) {
 
     if (val[0] == $("input:first").val()) {
@@ -114,12 +192,17 @@ $("#countryform").submit(function() {
         context: 'graphdata',
         dataType: 'json',
         success: function(o){
-            data_array = o;
-         },
+
+        data_array = o;
+        x = streamGraphTheData(data_array);
+			  transition(x);	
+    },
          error: function (xhr, ajaxOptions, thrownError){
              console.log(thrownError);
          }
     });
+
+
 
     labelsurl = '/streamgraph/' + countrynid + '/labels/';
     labels_array = {};
@@ -131,20 +214,23 @@ $("#countryform").submit(function() {
         dataType: 'json',
         success: function(o){
             labels_array = o;
-            showChart();
-						$("#chart-title").text(countrytitle);
+            showChartTitle();
+						$("#chart-title h1").text(countrytitle);
          },
-        complete: function(o){
-            hideLoadingGif();
+           complete: function(o){
+           hideLoadingGif();
          },
          error: function (xhr, ajaxOptions, thrownError){
-             console.log(thrownError);
+           console.log(thrownError);
          }
     });
 	  }
 });
   return false;
 });
+
+
+
 
 });
 
@@ -185,7 +271,3 @@ function stream_index(d, i) {
 	
   return {x: i, y: Math.max(0, d)};
 }
-
-/*data1 = d3.layout.stack().offset("wiggle")(x)*/
-
-/*transition()*/
